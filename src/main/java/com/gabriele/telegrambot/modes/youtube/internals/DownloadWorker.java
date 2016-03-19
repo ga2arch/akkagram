36c76378fdd661b8711bbfb3cf0122afef9a5673
ga2arch <gabriele.carrettoni@gmail.com>
2016-03-19 13:20:14 +0100

    little fixes

--- src/main/java/com/gabriele/telegrambot/modes/youtube/internals/DownloadWorker.java
+++ src/main/java/com/gabriele/telegrambot/modes/youtube/internals/DownloadWorker.java
@@ -1,6 +1,8 @@
 package com.gabriele.telegrambot.modes.youtube.internals;
 
 import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import com.gabriele.telegrambot.Bot;
 import com.gabriele.telegrambot.modes.youtube.messages.DownloadCompleted;
 import com.gabriele.telegrambot.modes.youtube.messages.DownloadMessage;
@@ -18,10 +20,11 @@ import java.util.UUID;
 
 public class DownloadWorker extends UntypedActor {
 
+    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
     @Override
     public void onReceive(Object in) throws Exception {
         if (in instanceof DownloadMessage) {
-
             startDownload(((DownloadMessage) in).getChatId(),
                     ((DownloadMessage) in).getLink(),
                     ((DownloadMessage) in).getJobId());
@@ -31,10 +34,11 @@ public class DownloadWorker extends UntypedActor {
 
     private void startDownload(String chatId, String url, String jobId) {
         try {
-
-            // TODO retrieve Song name and performer
+            log.debug(jobId + ") Fetching title for: " + url);
             Process process = new ProcessBuilder(
                     "youtube-dl",
+                    "-o",
+                    "%(title)s.%(ext)s",
                     "--get-filename",
                     url
             ).start();
@@ -43,19 +47,28 @@ public class DownloadWorker extends UntypedActor {
             String stderr = Okio.buffer(Okio.source(process.getErrorStream())).readUtf8();
 
             if (!stderr.isEmpty()) {
+                log.error(jobId + ") " + stderr);
                 Bot.getInstance().sendMessage(chatId, stderr);
                 getSender().tell(new DownloadError(jobId, url, stderr), getSelf());
 
             } else {
-                String filename = stdin.substring(0, stdin.lastIndexOf(".")) + ".mp3";
+                String title = stdin.substring(0, stdin.lastIndexOf("."));
+                String filename = title + ".mp3";
                 Bot.getInstance().sendMessage(chatId, "Downloading:\n" + filename);
+
+                log.info(jobId + ") Found title: " + title);
+                log.info(jobId + ") Starting download");
 
                 ProcessBuilder dlBuilder = new ProcessBuilder(
                         "youtube-dl",
                         "-x",
                         "--audio-format",
                         "mp3",
+                        "-f",
+                        "'best[filesize<50M]'",
                         url);
+
+                log.info(jobId + ") Download finished");
 
                 Path dlFolder = Files.createDirectories(Paths.get("static", UUID.randomUUID().toString()));
                 try {
@@ -65,13 +78,13 @@ public class DownloadWorker extends UntypedActor {
                     Path file = dlFolder.resolve(filename);
                     SendResponse resp = Bot.getInstance().sendAudio(chatId,
                             InputFile.audio(file.toFile()),
-                            null, null, null, null, null);
+                            null, null, title, null, null);
 
                     String fileId = resp.message().audio().fileId();
                     getSender().tell(new DownloadCompleted(jobId, fileId, url), getSelf());
 
                 } catch (RetrofitError e) {
-                    getSender().tell(new DownloadError(jobId, url, "File too big"), getSelf());
+                    getSender().tell(new DownloadError(jobId, url, e.getMessage()), getSelf());
                     e.printStackTrace();
 
                 } finally {
